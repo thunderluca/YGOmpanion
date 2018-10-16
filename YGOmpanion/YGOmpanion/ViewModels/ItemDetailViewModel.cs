7d0bd4c8731fc0833073cc664771bca30c259249
Luca Montanari <luca.montanari@selecta.it>
2018-10-16 11:30:50 +0200

    Add project files.

---
+++ YGOmpanion/YGOmpanion/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using YGOmpanion.Models;
+
+namespace YGOmpanion.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ YGOmpanion/YGOmpanion/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using YGOmpanion.Models;
+using YGOmpanion.ViewModels;
+
+namespace YGOmpanion.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ItemDetailPage : ContentPage
+    {
+        ItemDetailViewModel viewModel;
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+
+            var item = new Item
+            {
+                Text = "Item 1",
+                Description = "This is an item description."
+            };
+
+            viewModel = new ItemDetailViewModel(item);
+            BindingContext = viewModel;
+        }
+    }
+}
